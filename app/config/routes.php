2c93d7d0c29b7b3bae29b8d3c2f997fb465e2bba
SSead <smailagicsead@gmail.com>
2024-02-01 00:15:01 +0100

    :tada: Initial commit

---
+++ app/config/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+use app\controllers\ApiExampleController;
+use flight\Engine;
+use flight\net\Router;
+
+/** 
+ * @var Router $router 
+ * @var Engine $app
+ */
+$router->get('/', function() use ($app) {
+	$app->render('welcome', [ 'message' => 'You are gonna do great things!' ]);
+});
+
+$router->get('/hello-world/@name', function($name) {
+	echo '<h1>Hello world! Oh hey '.$name.'!</h1>';
+});
+
+$router->group('/api', function() use ($router, $app) {
+	$Api_Example_Controller = new ApiExampleController($app);
+	$router->get('/users', [ $Api_Example_Controller, 'getUsers' ]);
+	$router->get('/users/@id:[0-9]', [ $Api_Example_Controller, 'getUser' ]);
+	$router->post('/users/@id:[0-9]', [ $Api_Example_Controller, 'updateUser' ]);
+});
